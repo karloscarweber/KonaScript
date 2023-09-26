7dffdfc0ead042a7da958cb8d6d3b10a2f854205
karloscarweber <me@kow.fm>
2023-09-26 17:55:29 -0600

    Refactor Lox/Environment a bit to be more Class like.

--- lox/Environment.lua
+++ lox/Environment.lua
@@ -1,18 +1,29 @@
 -- lox/Environment.lua
 
--- The Environment variable thing
-LoxEnvironment = function()
-  local t = {}
-  t.values = {}
-  t.define = function(name, value)
-    t.values[name] = value
+-- The Environment of Lox is stored in a table.
+Environment = {}
+
+-- create a new Lox environment
+function Environment:new()
+  local environment = {}
+  environment.values = {}
+
+  setmetatable(environment, self)
+  self.__index = self
+
+  return environment
+end
+
+-- define a name with a value in the lox environment
+function Environment:define(name, value)
+  self.values[name] = value
+end
+
+-- get the value stored at name in the lox environment
+function Environment:get(name)
+  if self.values[name.lexeme] == nil then
+    error("Undefined Variable '" .. name.lexeme .. "'.")
+  else
+    return self.values[name.lexeme]
   end
-  t.get = function(name)
-    if t.values[name.lexeme] == nil then
-      error("Undefined Variable '" .. name.lexeme .. "'.")
-    else
-      return t.values[name.lexeme]
-    end
-  end
-  return t
 end
