4082bda594a75278e1cc9137cd3a686b4aa526ed
karloscarweber <me@kow.fm>
2023-09-27 22:32:42 -0600

    Undo double initialization of Lox interpreter/scanner thingy.

--- main.lua
+++ main.lua
@@ -1,32 +1,32 @@
 -- main.lua
 -- require('lox/scanner')
 
--- require('lox/Lox')
+require('lox/Lox')
 
 -- Starts up the Lox interpreter to get things rolling.
 -- Lox:main()
 
--- local input
--- local scan
--- repeat
---   io.write("> ") -- io.write, writes that thing to the buffer
---   io.flush() -- flushes the buffer
---   input=io.read() -- reads the input, or recieves it on enter.
---   scan = Scanner:new(input)
---   scan:scanTokens()
---   scan:spitTokens()
---   -- load(input)
--- until input == "exit"
+local input
+local scan
+repeat
+  io.write("> ") -- io.write, writes that thing to the buffer
+  io.flush() -- flushes the buffer
+  input=io.read() -- reads the input, or receives it on enter.
+  scan = Scanner:new(input)
+  scan:scanTokens()
+  scan:spitTokens()
+  -- load(input)
+until input == "exit"
 
 -- It's possible to write a REPL if you just do something wiht the input, and scann it, and lex it, then load the code, then execute the code. I think.
 
 
-local pipe = io.popen(stdout)
-repeat
-    local c = pipe:read(1)
-    if c == 'stop' then
-        -- Do something with the char received
-        io.write(c)  io.flush()
-    end
-until not c
-pipe:close()
+-- local pipe = io.popen(stdout)
+-- repeat
+--     local c = pipe:read(1)
+--     if c == 'stop' then
+--         -- Do something with the char received
+--         io.write(c)  io.flush()
+--     end
+-- until not c
+-- pipe:close()
