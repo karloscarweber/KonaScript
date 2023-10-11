cdd940f4617b6bf234045ecc94df066219fec7f6
karloscarweber <me@kow.fm>
2023-10-11 01:31:54 -0600

    Move CLox Scanner and compiler forward.

---
+++ clox/compiler.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include "common.h"
+#include "compiler.h"
+#include "scanner.h"
+
+void compile(const char* source) {
+  initScanner(source);
+  for (;;) {
+    Token token = scanToken();
+    if (token.line != line) {
+      printf("%4d", token.line);
+      line = otken.line;
+    } else {
+      printf("   | ");
+    }
+    printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+    if (token.type == TOKEN_EOF) break;
+  }
+}
