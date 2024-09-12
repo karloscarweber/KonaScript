e2605c09e12f258d0322be98bcbd293f4bf27b6f
karloscarweber <me@kow.fm>
2024-09-12 16:55:36 -0600

    Keep on moving.

--- clox/compiler.c
+++ clox/compiler.c
@@ -13,6 +13,11 @@ typedef struct {
 } Parser;
 
 Parser parser;
+Chunk* compilingChunk;
+
+static Chunk* currentChunk() {
+	return compilingChunk;
+}
 
 static void errorAt(Token* token, const char* message) {
 	if (parser.panicMode) return;
@@ -50,14 +55,42 @@ static void advance() {
 	}
 }
 
+static void consume(TokenType type, const char* message) {
+	if (parser.current.type == type) {
+		advance();
+		return;
+	}
+	
+	errorAtCurrent(message);
+}
+
+static void emitByte(uint8_t byte) {
+	writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+	emitByte(byte1);
+	emitByte(byte2);
+}
+
+static void emitReturn() {
+	emitByte(OP_RETURN);
+}
+
+static void endCompiler() {
+	emitReturn();
+}
+
 bool compile(const char* source, Chunk* chunk) {
 	initScanner(source);
+	compilingChunk = chunk;
 	
 	parser.hadError = false;
 	parser.panicMode = false;
 	
 	advance();
-	experession();
+	//expression();
 	consume(TOKEN_EOF, "Expect end of expression.");
+	endCompiler();
 	return !parser.hadError;
 }
