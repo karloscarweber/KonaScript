84cdeb432176c43a083915ccb236a7f81a76978a
karloscarweber <me@kow.fm>
2024-09-12 16:39:50 -0600

    working on the clox compiler.

--- clox/compiler.c
+++ clox/compiler.c
@@ -1,22 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char* source) {
-	initScanner(source);
-	int line = -1;
-	for (;;) {
-		Token token = scanToken();
-		if (token.line != line) {
-			printf("%4d ", token.line);
-			line = token.line;
-		} else {
-			printf("   | ");
-		}
-		printf("%2d '%.*s'\n", token.type, token.length, token.start);
-		
-		if (token.type == TOKEN_EOF) break;
+typedef struct {
+	Token current;
+	Token previous;
+	bool hadError;
+	bool panicMode;
+} Parser;
+
+Parser parser;
+
+static void errorAt(Token* token, const char* message) {
+	if (parser.panicMode) return;
+	parser.panicMode = true;
+	fprintf(stderr, "[line %d] Error", token->line);
+	
+	if (token->type == TOKEN_EOF) {
+		fprintf(stderr, " at end");
+	} else if (token->type == TOKEN_ERROR) {
+		// Nothing.
+	} else {
+		fprintf(stderr, " at '%.*s'", token->length, token->start);
 	}
+	
+	fprintf(stderr, ": %s\n", message);
+	parser.hadError = true;
+}
+
+static void error(const char* message) {
+	errorAt(&parser.previous, message);
+}
+
+static void errorAtCurrent(const char* message) {
+	errorAt(&parser.current, message);
+}
+
+static void advance() {
+	parser.previous = parser.current;
+	
+	for (;;) {
+		parser.current = scanToken();
+		if (parser.current.type != TOKEN_ERROR) break;
+		
+		errorAtCurrent(parser.current.start);
+	}
+}
+
+bool compile(const char* source, Chunk* chunk) {
+	initScanner(source);
+	
+	parser.hadError = false;
+	parser.panicMode = false;
+	
+	advance();
+	experession();
+	consume(TOKEN_EOF, "Expect end of expression.");
+	return !parser.hadError;
 }
