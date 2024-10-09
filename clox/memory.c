50948c1859aa5bb1e50ea40e37fb1af0ccc9f437
karloscarweber <me@kow.fm>
2024-10-09 00:47:43 -0600

    Got classes working.

--- clox/memory.c
+++ clox/memory.c
@@ -72,6 +72,11 @@ static void blackenObject(Obj* object) {
 #endif
 	
 	switch (object->type) {
+		case OBJ_CLASS: {
+			ObjClass* klass = (ObjClass*)object;
+			markObject((Obj*)klass->name);
+			break;
+		}
 		case OBJ_CLOSURE: {
 			ObjClosure* closure = (ObjClosure*)object;
 			markObject((Obj*)closure->function);
@@ -84,6 +89,12 @@ static void blackenObject(Obj* object) {
 			ObjFunction* function = (ObjFunction*)object;
 			markObject((Obj*)function->name);
 			markArray(&function->chunk.constants);
+		}
+		case OBJ_INSTANCE: {
+			ObjInstance* instance = (ObjInstance*)object;
+			markObject((Obj*)instance->klass);
+			markTable(&instance->fields);
+			break;
 		}
 		case OBJ_UPVALUE:
 			markValue(((ObjUpvalue*)object)->closed);
@@ -99,6 +110,10 @@ static void freeObject(Obj* object) {
 	printf("%p free type %d\n", (void*)object, object->type);
 #endif
 	switch (object->type) {
+		case OBJ_CLASS: {
+			FREE(ObjClass, object);
+			break;
+		}
 		case OBJ_CLOSURE: {
 			ObjClosure* closure = (ObjClosure*)object;
 			FREE_ARRAY(ObjUpvalue*, closure->upvalues, closure->upvalueCount);
@@ -111,6 +126,11 @@ static void freeObject(Obj* object) {
 			FREE(ObjFunction, object);
 			break;
 		}
+		case OBJ_INSTANCE:
+			ObjInstance* instance = (ObjInstance*)object;
+			freeTable(&instance->fields);
+			FREE(ObjInstance, object);
+			break;
 		case OBJ_NATIVE:
 			FREE(ObjNative, object);
 			break;
